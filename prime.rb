8feb9e3ad67ab0851db59ee5e94a5abd4690536d
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-16 09:52:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  else
+  (2..(integer-1)).each do |i|
+      return false if integer % i == 0
+    end
+  true
+
+end
+
+end
