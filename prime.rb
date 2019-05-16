0d0bc7f056aa622bcd0d3e47900a266abf299c97
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-16 09:44:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
 def prime?(integer)
-  if integer <= 1
-    return false
-  else 
+  for num in 2..(integer - 1)
+    if (integer % num) == 0
+      return false
+    end
+  end
 end
